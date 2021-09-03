71ce270c26f1a03b47798b9ea9390e39e8d7b180
Candice <candice.vernacchio@gmail.com>
2021-09-03 16:48:30 -0600

    commit tree 594bbd5d95f49861cf7cc03befc91fdd137d0642

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 31
+let num2 = 2
+const multiply = num1 * num2
 
-// Write your code here
+let random = Math.floor(Math.random() +1)
+
+let num3 = 3
+let num4 = 1
+const mod = num3 + num4
+
+let max = Math.max(0, 20)
